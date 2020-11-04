d69213a8ee2bd0a0e421c1f9bad009e8867f54b6
Nate Dunlap <nadunlap@microsoft.com>
2020-11-03 16:43:56 -0800

    test storybook

---
+++ precache-manifest.b7f5e4cea374518b243f5ee5ceb9a25d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae6a6ddea64a3875389fd2e6d0d0bbd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4d70a02cce80f4ebf342.bundle.js"
+  },
+  {
+    "url": "runtime~main.4d70a02cce80f4ebf342.bundle.js"
+  },
+  {
+    "url": "vendors~main.4d70a02cce80f4ebf342.bundle.js"
+  },
+  {
+    "url": "vendors~main.4d70a02cce80f4ebf342.bundle.js.LICENSE.txt"
+  }
+]);
